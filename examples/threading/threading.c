0b665d74ccf661a4d695b690d31068e9416ef530
robertharvey1 <robertharvey1@yahoo.com>
2025-01-05 11:44:41 +1300

    Updated From assignment 4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -36,7 +36,6 @@ void* threadfunc(void* thread_param)
     return thread_param;
 }
 
-
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
